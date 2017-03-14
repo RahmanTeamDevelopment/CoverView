90510128b49c5deed4c565bb982c579cf825a80f
Andrew Rimmer <andyrimmer@gmail.com>
2017-03-14 11:46:29 +0000

    Added function for writing multiple reads. Fixed pep8 errors. Tweaked Makefile

--- bamgen/bamgen.py
+++ bamgen/bamgen.py
@@ -1,20 +1,26 @@
+import array
 import pysam
 import sys
 
 
-def write_read_to_bam(bam, seq, qual, chrom, pos, map_qual, cigar, read_id):
+HIGH_QUAL = 100
+
+
+def write_perfect_unpaired_read_to_bam(bam, chrom, pos, seq, read_id):
     """
-    Write a single read to the specified BAM file.
+    Write a single perfect (i.e. no mis-matches or alignment gaps, is not a
+    duplicate, all bases are high quality, and the mapping quality is high)
+    un-paired read to the specified BAM file.
     """
     read = pysam.AlignedRead()
 
     read.query_name = "simulated_read_{}".format(read_id)
     read.query_sequence = seq
-    read.query_qualities = pysam.qualitystring_to_array(qual)
-    read.reference_id = chrom
+    read.query_qualities = array.array('b', [HIGH_QUAL]*len(seq))
+    read.reference_id = bam.get_tid(chrom)
     read.reference_start = pos
-    read.mapping_quality = map_qual
-    read.cigar = cigar
+    read.mapping_quality = HIGH_QUAL
+    read.cigar = ((0, len(seq)),)  # Perfect match
     read.next_reference_id = -1
     read.next_reference_start = -1
     read.is_unmapped = 0
@@ -24,32 +30,41 @@ def write_read_to_bam(bam, seq, qual, chrom, pos, map_qual, cigar, read_id):
     bam.write(read)
 
 
-if __name__ == "__main__":
-    ref_file = pysam.Fastafile(sys.argv[1])
+def write_n_perfect_unpaired_reads_to_bam(bam, ref, chrom, pos, length,
+                                          read_id, n):
+    """
+    Write a number of identical, perfect (i.e. no mis-matches or alignment
+    gaps, is not a duplicate, all bases are high quality, and the mapping
+    quality is high) un-paired reads to the specified BAM file.
+    """
+    seq = ref.fetch(chrom, pos, pos+length)
 
-    bam_file = pysam.Samfile(
+    for i in xrange(n):
+        write_perfect_unpaired_read_to_bam(bam, chrom, pos, seq, read_id + i)
+
+
+if __name__ == "__main__":
+    ref_file = pysam.FastaFile(sys.argv[1])
+
+    bam_file = pysam.AlignmentFile(
         "test.bam",
         'wb',
         reference_names=ref_file.references,
         reference_lengths=ref_file.lengths
     )
 
-    for i in xrange(10000):
-        seq = "AGCTTAGCTAGCTACCTATATCTTGGTCTTGGCCG"
-        qual = "<"*len(seq)
-        tid = 0  # chr1
-        start_pos = 32
-        map_qual = 50
-        cigar = ((0, 10),)
+    seq = "AGCTTAGCTAGCTACCTATATCTTGGTCTTGGCCG"
+    chrom = "1"
+    start_pos = 32
+    read_length = 100
 
-        write_read_to_bam(
-            bam_file,
-            seq,
-            qual,
-            tid,
-            start_pos,
-            map_qual,
-            cigar,
-            i)
+    write_n_perfect_unpaired_reads_to_bam(
+        bam_file,
+        ref_file,
+        chrom,
+        start_pos,
+        read_length,
+        0,
+        10000)
 
     bam_file.close()
