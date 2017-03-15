792116ea0c62d4da756e22229f43f20ba2590d1b
Andrew Rimmer <andyrimmer@gmail.com>
2017-03-15 16:57:34 +0000

    Added a couple of simple tests

--- test/test_bamgen.py
+++ test/test_bamgen.py
@@ -1,9 +1,15 @@
 import bamgen
+import os
 import pysam
 import unittest
 
 
-class TestBamGen(unittest.TestCase):
+class TestSimpleBamFileGeneration(unittest.TestCase):
+
+    def tearDown(self):
+        os.remove("test1.bam")
+        os.remove("test1.bam.bai")
+        
     def test_can_generate_bam_file_with_single_read(self):
         config = {
             "bam_files":
@@ -28,6 +34,58 @@ class TestBamGen(unittest.TestCase):
 
         with pysam.AlignmentFile("test1.bam", 'rb') as bam_file:
             assert bam_file.count("1", 0, 100) == 1
+
+    def test_can_generate_bam_file_with_two_reads(self):
+        config = {
+            "bam_files":
+                [
+                    {
+                        "file_name": "test1.bam",
+                        "reference": "/Users/arimmer/Work/Reference/Reference/human_g1k_v37.fa",
+                        "reads":
+                        [
+                            {
+                                "chrom": "1",
+                                "start_pos": 32,
+                                "read_length": 100,
+                                "num_reads": 2
+                            }
+                        ]
+                    }
+                ]
+        }
+
+        bamgen.generate_bam_files(config)
+
+        with pysam.AlignmentFile("test1.bam", 'rb') as bam_file:
+            assert bam_file.count("1", 0, 100) == 2
+
+    def test_can_generate_bam_file_with_many_reads(self):
+        config = {
+            "bam_files":
+                [
+                    {
+                        "file_name": "test1.bam",
+                        "reference": "/Users/arimmer/Work/Reference/Reference/human_g1k_v37.fa",
+                        "reads":
+                        [
+                            {
+                                "chrom": "1",
+                                "start_pos": 32,
+                                "read_length": 100,
+                                "num_reads": 1000
+                            }
+                        ]
+                    }
+                ]
+        }
+
+        bamgen.generate_bam_files(config)
+
+        with pysam.AlignmentFile("test1.bam", 'rb') as bam_file:
+            assert bam_file.count("1", 0, 100) == 1000
+
+
 
 
 if __name__ == "__main__":
