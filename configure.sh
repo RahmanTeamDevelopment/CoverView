828ffcdcb2d6333df5475b96ce542e1382857d89
Andrew Rimmer <andyrimmer@gmail.com>
2017-05-04 16:30:33 +0100

    Upgrade pip before installing anything else

--- configure.sh
+++ configure.sh
@@ -2,5 +2,6 @@
 
 virtualenv -p python2.7 env
 source env/bin/activate
+pip install -U pip
 pip install -r requirements.txt --no-cache-dir --ignore-installed
 pip install . --no-cache-dir --ignore-installed
