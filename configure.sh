51b38bc830b999204f8f68097cadc1f19fe1e831
Andrew Rimmer <andyrimmer@gmail.com>
2017-05-04 09:28:28 +0100

    Disabling pip cache. I think this is sometimes causing installation problems when conflicting wheels exist on the users system

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@
 
 virtualenv -p python2.7 env
 source env/bin/activate
-pip install -r requirements.txt
-pip install -e .
+pip install -r requirements.txt --no-cache-dir
+pip install -e . --no-cache-dir
