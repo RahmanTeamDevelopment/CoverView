53f3bfa993363c5fd6fa834e12ce9124a30db018
Andrew Rimmer <andyrimmer@gmail.com>
2017-05-04 10:38:32 +0100

    Now telling pip to ignore any already-installed packages, and re-install anyway

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@
 
 virtualenv -p python2.7 env
 source env/bin/activate
-pip install -r requirements.txt --no-cache-dir
+pip install -r requirements.txt --no-cache-dir --ignore-installed
 pip install -e . --no-cache-dir
