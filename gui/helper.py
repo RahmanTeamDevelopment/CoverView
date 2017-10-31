a230312ca01ab86eab8fe0b160fad7aa0b695228
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-10-31 17:23:57 +0100

    GUI: SNPs are not shown in the Gene View and not counted in number of failed genes

--- gui/helper.py
+++ gui/helper.py
@@ -1,14 +1,26 @@
 
+def region_size(region_name, region_coords):
+    x = region_coords[region_name]
+    return int(x[2])-int(x[1])
+
 def create_fail_statistics(regions_data, region_coords):
 
     by_gene = {}
     counter_failed_regions = 0
     for r in regions_data:
         region_name = r['region']
-        gene_name = region_name.split('_')[0]
+
+        if '_' in region_name:
+            gene_name = region_name.split('_')[0]
+        else:
+            gene_name = region_name
+
         fail = (r['pass_or_fail'] == 'FAIL')
         if fail:
             counter_failed_regions += 1
+
+        if region_size(region_name, region_coords) == 1:
+            continue
 
         if gene_name not in by_gene:
             by_gene[gene_name] = {
@@ -39,5 +51,3 @@ def create_fail_statistics(regions_data, region_coords):
             counter_failed_genes += 1
 
     return all_genes, by_chrom, '{}  [of {} regions]'.format(counter_failed_regions, len(regions_data)), '{}  [of {} genes]'.format(counter_failed_genes, len(by_gene))
-
-
